71fc158f559e8fd180220d1b2aa390a6cd96b6be
d-_-b-Latitude <vcs@ludo.co.za>
2019-08-10 13:29:04 -0500

    Moved TMQTTGate code into TMQTTThread

--- cheeapp/mqttmodule.pas
+++ cheeapp/mqttmodule.pas
@@ -5,9 +5,64 @@ unit mqttmodule;
 interface
 
 uses
-  Classes, SysUtils, SyncObjs, MQTT, FPTimer;
+  Classes, SysUtils, SyncObjs, MQTT, FPTimer, ezutil;
 
 type
+
+  { TMQTTThread }
+
+  TMQTTThread = class(TThread)
+    private
+      fServer: string;
+      fPort: Integer;
+      fUsername: string;
+      fPassword: string;
+
+      fSyncCode: TCriticalSection;
+      fMQTTClient: TMQTTClient;
+
+      fTimerTick: TFPTimer;
+      fAliveCount: Integer;
+      fAliveCountDelay: Integer;
+      fAliveTopic: String;
+
+      fTopics: TStringList;
+
+      fLastWriteDebugMessage: string;
+      fOnWriteDebug: TWriteDebug;
+
+      fAutoReconnect: Boolean;
+
+      procedure SetupClient;
+      procedure WriteDebugSync;
+      procedure WriteDebug(aStr: string);
+
+      // Unsafe events! Called from MQTT thread (TMQTTReadThread)
+      procedure OnConnAck(Sender: TObject; ReturnCode: integer);
+      procedure OnPingResp(Sender: TObject);
+      procedure OnSubAck(Sender: TObject; MessageID: integer; GrantedQoS: integer);
+      procedure OnUnSubAck(Sender: TObject; MessageID: integer);
+      procedure OnMessage(Sender: TObject; topic, payload: TMqttString; isRetain: boolean);
+
+      procedure WaitForConnection;
+      procedure AfterConnect;
+      procedure BeforeDisconnect;
+      procedure CreateAndResetAliveTimer;
+      procedure SubscribeToTopics;
+      procedure UnSubscribeToTopics;
+      procedure OnTimerTick(Sender: TObject);
+      procedure GetMessageLoop;
+
+      procedure ConnectAndLoop;
+      procedure Disconnect;
+    protected
+      procedure Execute; override;
+    public
+      Constructor Create(CreateSuspended : boolean);
+      destructor Destroy; override;
+      property OnWriteDebug: TWriteDebug read fOnWriteDebug write fOnWriteDebug;
+      property AutoReconnect: Boolean read fAutoReconnect write fAutoReconnect;
+    end;
 
   { TMQTTGate }
 
@@ -15,42 +70,15 @@ type
     procedure DataModuleCreate(Sender: TObject);
     procedure DataModuleDestroy(Sender: TObject);
   private
-    fServer: string;
-    fPort: Integer;
-    fUsername: string;
-    fPassword: string;
-
-    fSyncCode: TCriticalSection;
-    fMQTTClient: TMQTTClient;
-
-    fTimerTick: TFPTimer;
-    fAliveCount: Integer;
-    fAliveCountDelay: Integer;
-    fAliveTopic: String;
-
-    fTopics: TStringList;
-
-    fExitMessageLoop: Boolean;
-
-    procedure SetupClient;
-    procedure WriteDebug(aStr: string);
-
-    // Unsafe events! Called from MQTT thread (TMQTTReadThread)
-    procedure OnConnAck(Sender: TObject; ReturnCode: integer);
-    procedure OnPingResp(Sender: TObject);
-    procedure OnSubAck(Sender: TObject; MessageID: integer; GrantedQoS: integer);
-    procedure OnUnSubAck(Sender: TObject; MessageID: integer);
-    procedure OnMessage(Sender: TObject; topic, payload: TMqttString; isRetain: boolean);
-
-    procedure WaitForConnection;
-    procedure OnConnected;
-    procedure CreateAndResetAliveTimer;
-    procedure SubscribeToTopics;
-    procedure OnTimerTick(Sender: TObject);
-    procedure GetMessageLoop;
+    fMQTTThread: TMQTTThread;
+    fOnWriteDebug: TWriteDebug;
+    function GetAutoReconnect: Boolean;
+    procedure SetAutoReconnect(AValue: Boolean);
   public
-
-    procedure Connect;
+    property AutoReconnect: Boolean read GetAutoReconnect write SetAutoReconnect;
+    property OnWriteDebug: TWriteDebug read fOnWriteDebug write fOnWriteDebug;
+    procedure Start;
+    procedure Stop;
   end;
 
 var
@@ -61,37 +89,46 @@ implementation
 {$R *.lfm}
 
 uses
-  DateUtils, ezutil, LazSysUtils;
+  DateUtils, LazSysUtils;
 
 { TMQTTGate }
 
 procedure TMQTTGate.DataModuleCreate(Sender: TObject);
 begin
-  // TODO remove hard coded testing var
-  fServer := 'planeteer.mooo.com';
-  fPort := 1883;
-  fUsername := 'rugraat';
-  fPassword := 'rugraat';
-  fAliveTopic:= 'alive';
-  fAliveCountDelay := 5000;
-
-  fTopics := TStringList.Create;
-  fTopics.add('#'); // TODO remove listen to all
-
-  fSyncCode := TCriticalSection.Create();
-  fMQTTClient := TMQTTClient.Create(fServer, fPort);
-  SetupClient;
-
+  fMQTTThread := TMQTTThread.Create(True);
 end;
 
 procedure TMQTTGate.DataModuleDestroy(Sender: TObject);
 begin
-  FreeAndNil(TimerTick);
-  FreeAndNil(MQTTClient);
-  FreeAndNil(SyncCode);
+  FreeAndNil(fMQTTThread);
 end;
 
-procedure TMQTTGate.SetupClient;
+function TMQTTGate.GetAutoReconnect: Boolean;
+begin
+  result := fMQTTThread.AutoReconnect;
+end;
+
+
+procedure TMQTTGate.SetAutoReconnect(AValue: Boolean);
+begin
+  fMQTTThread.AutoReconnect := AValue;
+end;
+
+
+procedure TMQTTGate.Start;
+begin
+  if Assigned(fOnWriteDebug) then
+    fMQTTThread.OnWriteDebug:= fOnWriteDebug;
+  fMQTTThread.Start;
+end;
+
+procedure TMQTTGate.Stop;
+begin
+  fMQTTThread.Terminate;
+
+end;
+
+procedure TMQTTThread.SetupClient;
 begin
   fMQTTClient.UserName := fUserName;
   fMQTTClient.Password := fPassword;
@@ -102,51 +139,58 @@ begin
   fMQTTClient.OnUnSubAck:= @OnUnSubAck;
 end;
 
-procedure TMQTTGate.WriteDebug(aStr: string);
+procedure TMQTTThread.WriteDebugSync;
 begin
-  // TODO
+  if Assigned(fOnWriteDebug) then
+  begin
+    fOnWriteDebug(fLastWriteDebugMessage);
+  end;
 end;
 
-procedure TMQTTGate.WaitForConnection;
+procedure TMQTTThread.WriteDebug(aStr: string);
+begin
+  fLastWriteDebugMessage:= aStr;
+  Synchronize(@WriteDebugSync);
+end;
+
+procedure TMQTTThread.WaitForConnection;
 begin
   while not fMQTTClient.isConnected do
   begin
     WriteDebug('Waiting for connection...');
-    //Sleep(1000);
-    // wait other thread
-    CheckSynchronize(1000);
+    Sleep(100);
   end;
 end;
 
-procedure TMQTTGate.OnConnAck(Sender: TObject; ReturnCode: integer);
+procedure TMQTTThread.OnConnAck(Sender: TObject; ReturnCode: integer);
 begin
   fSyncCode.Enter;
   WriteDebug('ConnAck');
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.OnPingResp(Sender: TObject);
+procedure TMQTTThread.OnPingResp(Sender: TObject);
 begin
   fSyncCode.Enter;
   WriteDebug('PingResp');
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.OnSubAck(Sender: TObject; MessageID: integer; GrantedQoS: integer);
+procedure TMQTTThread.OnSubAck(Sender: TObject; MessageID: integer; GrantedQoS: integer);
 begin
   fSyncCode.Enter;
   WriteDebug('SubAck');
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.OnUnSubAck(Sender: TObject; MessageID: integer);
+procedure TMQTTThread.OnUnSubAck(Sender: TObject; MessageID: integer);
 begin
   fSyncCode.Enter;
   WriteDebug('UnSubAck');
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.OnMessage(Sender: TObject; topic, payload: TMqttString;
+procedure TMQTTThread.OnMessage(Sender: TObject; topic, payload: TMqttString;
   isRetain: boolean);
 begin
   fSyncCode.Enter;
@@ -154,13 +198,18 @@ begin
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.OnConnected;
+procedure TMQTTThread.AfterConnect;
 begin
   CreateAndResetAliveTimer;
   SubscribeToTopics;
 end;
 
-procedure TMQTTGate.CreateAndResetAliveTimer;
+procedure TMQTTThread.BeforeDisconnect;
+begin
+  FreeAndNil(fTimerTick);
+end;
+
+procedure TMQTTThread.CreateAndResetAliveTimer;
 begin
   fAliveCount := 0;
   if assigned(fTimerTick) then
@@ -168,7 +217,7 @@ begin
   fTimerTick := NewTimer(fAliveCountDelay, @OnTimerTick, true);
 end;
 
-procedure TMQTTGate.SubscribeToTopics;
+procedure TMQTTThread.SubscribeToTopics;
 var
   i: integer;
 begin
@@ -179,7 +228,21 @@ begin
   end;
 end;
 
-procedure TMQTTGate.OnTimerTick(Sender: TObject);
+procedure TMQTTThread.UnSubscribeToTopics;
+var
+  i: integer;
+begin
+  for i := 0 to pred(fTopics.Count) do
+  begin
+    WriteDebug('Unsubscribe: "' + fTopics[i] + '"');
+    if assigned(fMQTTClient) and fMQTTClient.isConnected then
+      fMQTTClient.Unsubscribe(fTopics[i])
+    else
+      WriteDebug('Disconnected CANNOT Unsubscribe: "' + fTopics[i] + '"');
+  end;
+end;
+
+procedure TMQTTThread.OnTimerTick(Sender: TObject);
 var
   vNow: TDateTime;
   vTimeStamp: integer;
@@ -194,16 +257,13 @@ begin
   fSyncCode.Leave;
 end;
 
-procedure TMQTTGate.GetMessageLoop;
+procedure TMQTTThread.GetMessageLoop;
 var
   vMsg: TMQTTMessage;
   vAckMsg: TMQTTMessageAck;
 begin
-  fExitMessageLoop := False;
-  while (not fExitMessageLoop) and (fMQTTClient.isConnected) do
+  while (not Terminated) and (fMQTTClient.isConnected) do
   begin
-   // wait other thread
-   CheckSynchronize;//(1000);
    vAckMsg := fMQTTClient.getMessageAck;
    if assigned(vAckMsg) then begin
      WriteDebug('AckMsg ' + inttostr(vAckMsg.messageId) + ' ' + inttostr(vAckMsg.returnCode));
@@ -217,10 +277,65 @@ begin
   end;
 end;
 
-procedure TMQTTGate.Connect;
+procedure TMQTTThread.ConnectAndLoop;
 begin
-  fMQTTClient.Connect();
+  fMQTTClient.Connect;
   WaitForConnection;
+  AfterConnect;
+  GetMessageLoop;
+end;
+
+procedure TMQTTThread.Disconnect;
+begin
+  BeforeDisconnect;
+  UnSubscribeToTopics;
+  fMQTTClient.Disconnect;
+  Sleep(100); // wait for disconnect msg to be sent
+  fMQTTClient.ForceDisconnect;
+end;
+
+procedure TMQTTThread.Execute;
+begin
+  ConnectAndLoop;
+  while (not Terminated) and fAutoReconnect do
+  begin
+    Disconnect;
+    ConnectAndLoop;
+
+  end;
+end;
+
+constructor TMQTTThread.Create(CreateSuspended: boolean);
+begin
+  inherited Create(CreateSuspended);
+  FreeOnTerminate := False;
+
+  // TODO remove hard coded testing var
+  fServer := 'planeteer.mooo.com';
+  fPort := 1883;
+  fUsername := 'rugraat';
+  fPassword := 'rugraat';
+  fAliveTopic:= 'alive';
+  fAliveCountDelay := 5000;
+  fAutoReconnect := True;
+
+  fTopics := TStringList.Create;
+  fTopics.add('#'); // TODO remove listen to all
+
+  fSyncCode := TCriticalSection.Create();
+  fMQTTClient := TMQTTClient.Create(fServer, fPort);
+  SetupClient;
+end;
+
+destructor TMQTTThread.Destroy;
+begin
+  Disconnect;
+  FreeAndNil(fTimerTick);
+  FreeAndNil(fMQTTClient);
+  FreeAndNil(fSyncCode);
+
+
+  inherited Destroy;
 end;
 
 
